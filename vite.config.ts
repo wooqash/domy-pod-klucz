7e64168dfc6fb7032817ec8d2144ca098c78e1fb
wooqash <lukasz@zibeline.pl>
2025-08-19 06:07:13 +0200

    basic vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,38 @@
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig } from 'vite';
+// import { readdirSync } from 'fs';
+// import { resolve } from 'path';
+// import path from "path";
+import type { UserConfig } from 'vite'
+
+// const htmlFiles = {};
+// readdirSync('./src/pages').forEach(file => {
+//   if (file.endsWith('.html')) {
+//     const name = file.replace('.html', '');
+//     htmlFiles[name] = resolve(__dirname, 'src/pages', file);
+//   }
+// });
+
+// export default defineConfig({
+//   base: "./",
+//   root: './src/pages/',
+//   build: {
+//     outDir: '../../dist',
+//     rollupOptions: {
+//       // input: getHtmlInputs('src/pages')
+//       input: htmlFiles
+//     }
+//   },
+//   // server: {
+//   //   // Wymuś serwowanie plików HTML z src/pages
+//   //   middlewareMode: true,
+//   // },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//       "@@": path.resolve(__dirname),
+//     },
+//   },
+// }) satisfies UserConfig;
 
 export default defineConfig({
   build: {
@@ -6,6 +40,7 @@ export default defineConfig({
       input: {
         main: './index.html',
         comingSoon: './coming-soon.html',
+        work: './work.html',
         // Dodaj kolejne pliki według potrzeb
       }
     }
