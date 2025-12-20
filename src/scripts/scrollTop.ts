72b62fb94cb46c04395d942f75b602a8d52cbda2
wooqash <lukasz@zibeline.pl>
2025-12-20 09:54:02 +0100

    refactored scrollTop script

--- src/scripts/scrollTop.ts
+++ src/scripts/scrollTop.ts
@@ -1,29 +1,16 @@
-import type { ScrollToTarget, ScrollToOptions } from "locomotive-scroll";
-
 const scrollTopBtn: HTMLButtonElement | null =
   document.querySelector("#scrollToTopBtn");
 const logoLink: HTMLAnchorElement | null = document.querySelector(".logo-link");
 const hero: HTMLElement | null = document.getElementById("hero");
 
 export const initScrollToTopBtn = (locomotiveScroll: LocomotiveScroll) => {
-  function scrollTo({
-    target,
-    options,
-  }: {
-    target: ScrollToTarget;
-    options?: ScrollToOptions;
-  }) {
-    // const { target, options } = params;
-    locomotiveScroll.scrollTo(target, options);
-  }
-
-  function isBelowHero() {
+  const isBelowHero = () => {
     if (!hero) return window.scrollY > 100;
     const heroBottom = hero.getBoundingClientRect().bottom + window.scrollY;
     return window.scrollY > heroBottom - 40;
-  }
+  };
 
-  function toggleBtn() {
+  const toggleBtn = () => {
     if (scrollTopBtn) {
       if (isBelowHero()) {
         scrollTopBtn.classList.add("show");
@@ -31,20 +18,17 @@ export const initScrollToTopBtn = (locomotiveScroll: LocomotiveScroll) => {
         scrollTopBtn.classList.remove("show");
       }
     }
-  }
+  };
 
-  logoLink?.addEventListener("click", e => {
+  const handleClick = (e: Event) => {
     e.preventDefault();
     if (hero) {
-      scrollTo({ target: hero });
+      locomotiveScroll.scrollTo(hero, {});
     }
-  });
-  scrollTopBtn?.addEventListener("click", e => {
-    e.preventDefault();
-    if (hero) {
-      scrollTo({ target: hero });
-    }
-  });
+  };
+
+  logoLink?.addEventListener("click", handleClick);
+  scrollTopBtn?.addEventListener("click", handleClick);
   locomotiveScroll.on("scroll", toggleBtn);
   window.addEventListener("resize", toggleBtn);
 };
